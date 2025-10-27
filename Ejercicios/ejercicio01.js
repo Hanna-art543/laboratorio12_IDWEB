4cbd839940c386a263f8d89e76960e8a83c7befb
Ana Miranda <amirandalo@unsa.edu.pe>
2025-10-27 16:19:30 -0500

    Subiendo ejercicio01.js

---
+++ Ejercicios/ejercicio01.js
@@ -0,0 +1,19 @@
+//Función es par
+function esPar(num) {
+    if (num % 2 === 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+//Ingresar número y mostrar resultado
+let numero = parseInt(prompt("Ingrese el número:"));
+
+if(esPar(numero)) {
+    alert("El número "+numero+" es par.");
+}
+else {
+    alert("El número es impar.")
+}
