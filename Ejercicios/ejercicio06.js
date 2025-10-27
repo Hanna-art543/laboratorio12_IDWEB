059c1902d02ceacd2d1cbfb61a46a1ed7f5be121
Ana Miranda <amirandalo@unsa.edu.pe>
2025-10-27 16:58:17 -0500

    Subir ejercicio06.js

---
+++ Ejercicios/ejercicio06.js
@@ -0,0 +1,9 @@
+//Función transformar a mayúsculas
+function convertirAMayusculas (texto) {
+    let textoMayusculas = texto.toUpperCase();
+    return textoMayusculas;
+}
+
+//Ingresar y mostrar
+let texto = prompt("Ingresar un texto:");
+alert(convertirAMayusculas(texto));
